cfb4482ef92e554122724f5919bb24225483bca6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-06-27 17:29:37 +0900

    gsonとgoogle play servicesを追加

--- src/com/buruburu/nabi/MainActivity.java
+++ src/com/buruburu/nabi/MainActivity.java
@@ -1,9 +1,18 @@
 package com.buruburu.nabi;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +20,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.d("buruburunabi", "aaaaaaaaaaaaa");
 	}
 }
